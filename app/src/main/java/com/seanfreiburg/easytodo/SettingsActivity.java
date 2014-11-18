0a3ec5c3281e23a17fdd9ca0d50adda42e8bc353
Sean Freiburg <frycicle@gmail.com>
2014-11-17 23:54:14 -0800

    add settings

---
+++ app/src/main/java/com/seanfreiburg/easytodo/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.seanfreiburg.easytodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+
+
+}
